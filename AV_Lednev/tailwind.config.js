f129aa1a6f9ab4f002084d27d6108cc0a9a45423
Trooper01k <tankzen228@gmail.com>
2023-12-19 19:36:40 +0300

    first

---
+++ AV_Lednev/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      padding:{
+        big:'50px'
+      }
+    },
+  },
+  plugins: [],
+}
+
